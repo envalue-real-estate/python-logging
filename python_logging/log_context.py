3c051b9eb5d1045aebe7a44aa308d20312827509
Theo van Oostrum <gt.oostrum@gmail.com>
2024-12-03 13:09:24 +0100

    default LogContext without request context

--- python_logging/log_context.py
+++ python_logging/log_context.py
@@ -4,11 +4,11 @@ from contextvars import ContextVar
 
 
 class LogContext:
-    def __init__(self, *, request_method: str, request_path: str):
+    def __init__(self, *, request_method: str | None = None, request_path: str | None = None):
         self._application = str(os.getenv('APPLICATION', default=None))
         self._request_id = str(uuid.uuid4())
-        self._request_method: str = request_method
-        self._request_path: str = request_path
+        self._request_method: str | None = request_method
+        self._request_path: str | None = request_path
         self._response_status_code: int | None = None
 
     def get_application(self):
@@ -26,6 +26,15 @@ class LogContext:
     def get_response_status_code(self) -> int:
         return self._response_status_code
 
+    def set_request_id(self, request_id: str):
+        self._request_id = request_id
+
+    def set_request_method(self, request_method: str):
+        self._request_method = request_method
+
+    def set_request_path(self, request_path: str):
+        self._request_path = request_path
+
     def set_response_status_code(self, code: int):
         self._response_status_code = code
 
@@ -38,4 +47,8 @@ def set_log_context(log_context: LogContext):
 
 
 def get_log_context() -> LogContext:
-    return _log_context_var.get()
+    log_context = _log_context_var.get()
+    if log_context is None:
+        _log_context_var.set(LogContext())
+        log_context = _log_context_var.get()
+    return log_context
