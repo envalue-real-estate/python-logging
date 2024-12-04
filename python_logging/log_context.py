36a1f3775dce1264564b28fc2d7316c265d8b581
Theo van Oostrum <gt.oostrum@gmail.com>
2024-12-04 15:57:40 +0100

    #628: application and environment as computed properties in LogContext because they need to be determined at runtime

--- python_logging/log_context.py
+++ python_logging/log_context.py
@@ -6,13 +6,19 @@ from dataclasses import dataclass
 
 @dataclass
 class LogContext:
-    application = str(os.getenv('APPLICATION', default=None))
-    environment = str(os.getenv('ENVIRONMENT', default=None))
     request_id = str(uuid.uuid4())
     request_method: str | None = None
     request_path: str | None = None
     user_agent: str | None = None
     response_status_code: int | None = None
+
+    @property
+    def application(self) -> str:
+        return os.getenv('APPLICATION', default=None)
+
+    @property
+    def environment(self) -> str:
+        return os.getenv('ENVIRONMENT', default='local')
 
 
 _log_context_var: ContextVar[LogContext | None] = ContextVar("log_context", default=None)
