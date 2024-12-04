22eecf2dc23f40cfc7c4b06d892b757bc2015ebc
Theo van Oostrum <gt.oostrum@gmail.com>
2024-12-04 13:05:19 +0100

    #628: Added environment and user_agent to logs

--- python_logging/formatters.py
+++ python_logging/formatters.py
@@ -13,13 +13,15 @@ class JsonFormatter(logging.Formatter):
             'level': record.levelname,
             'message': record.getMessage(),
             'application': log_context.application,
+            'environment': log_context.environment,
             'request_id': log_context.request_id,
         }
 
         if log_context.request_path is not None:
             log_object['request'] = {
                 'method': log_context.request_method,
-                'path': log_context.request_path
+                'path': log_context.request_path,
+                'user_agent': log_context.user_agent
             }
         if log_context.response_status_code is not None:
             log_object['response'] = {
